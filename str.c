3fa8d099c5cb0138a1c2802dfcd39630078ce5b3
Nguyễn N. Thiện <kakangocthien109@gmail.com>
2021-09-15 02:34:05 +0700

    Asm

--- str.c
+++ str.c
@@ -14,10 +14,10 @@ int main() {
     char str2[] = "nguyennt.dev";
     char str3[] = "quocthinhvo.dev";
 
-    printf("Ten thanh vien moi cua Kode: %s""\n", str2);
-    strcpy(str2, str3);
+    printf("Ten thanh vien moi cua Kode: %s, ten thanh vien cu cua Kode: %s""\n", str2, str3); // Print the original str2 and str3
+    strcpy(str2, str3); // Copy the value from str3 to str2 (Structure: strcpy(dest, src);)
     printf("1 month after... \n");
-    printf("Ten thanh vien cu cua Kode: %s", str3);
+    printf("Ten thanh vien cu cua Kode: %s, ten thanh vien cu cua Kode: %s", str2, str3); // Print the modified str2
 
    return 0; 
 }
