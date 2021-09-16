bb9101ff9c566b28aab3ee91e86847946a4e6213
Nguyễn N. Thiện <kakangocthien109@gmail.com>
2021-09-16 21:09:13 +0700

    ASSM

--- Labs/lab06.1.c
+++ Labs/lab06.1.c
@@ -9,11 +9,11 @@ int main()
 
     printf("Cac so chan co tu 1 den %d la: ", a);
 
-    for (int i = 0; i < a; i++)
+    for (int i = 0; i <= a; i++)
     {
         if (i % 2 == 0)
         {
-            printf("%d\n", i);
+            printf("%d | ", i);
         }
     }
 
